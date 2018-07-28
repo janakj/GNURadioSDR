73cfffaef577230aa3541c1bef59354f409a4bf1
Jan Janak <jan@janakj.org>
2018-07-28 12:03:41 -0400

    Create hierarchical CODEC2 encoder and decoder.

--- p25_rx_udp.py
+++ p25_rx_udp.py
@@ -12,15 +12,10 @@ from gnuradio import vocoder
 from p25_config import DEFAULT_PORT, CODEC2_MODE, CODEC2_BITS_PER_FRAME, DYNAMIC_RANGE, SAMPLE_RATE
 
 
-class p25_rx_udp(gr.top_block):
-
-    def create_blocks(self):
-        self.socket_pdu = blocks.socket_pdu("UDP_SERVER", '0.0.0.0', self.port, 10000, False)
-
-        # ConvertUDP datagrams into a stream of tagged value, each
-        # value will contain the contents of the UDP datagram. The
-        # contents of the datagram is one packed CODEC2 frame.
-        self.pdu_to_tagged_stream = blocks.pdu_to_tagged_stream(blocks.byte_t, 'packet_len')
+class codec2_decoder(gr.hier_block2):
+    def __init__(self):
+        gr.hier_block2.__init__(self, "codec2", gr.io_signature(1, 1, gr.sizeof_char),
+                                gr.io_signature(1, 1, gr.sizeof_float))
 
         # Unpack the 8 bytes (one codec2 frame) into a stream of bits
         self.unpacker = blocks.packed_to_unpacked_bb(1, gr.GR_LSB_FIRST)
@@ -33,16 +28,34 @@ class p25_rx_udp(gr.top_block):
         # Scale the dynamic range of the stream back to [-1, 1] and
         # feed it to the soundcard.
         self.short_to_float = blocks.short_to_float(1, DYNAMIC_RANGE)
+
+        self.connect(self,
+                     self.unpacker,
+                     self.stream_to_vector,
+                     self.codec2_decode,
+                     self.short_to_float,
+                     self)
+
+
+class p25_rx_udp(gr.top_block):
+
+    def create_blocks(self):
+        self.socket_pdu = blocks.socket_pdu("UDP_SERVER", '0.0.0.0', self.port, 10000, False)
+
+        # ConvertUDP datagrams into a stream of tagged value, each
+        # value will contain the contents of the UDP datagram. The
+        # contents of the datagram is one packed CODEC2 frame.
+        self.pdu_to_tagged_stream = blocks.pdu_to_tagged_stream(blocks.byte_t, 'packet_len')
+
+        self.codec2_decoder = codec2_decoder()
+
         self.audio_sink = audio.sink(SAMPLE_RATE, '', True)
 
 
     def create_connections(self):
         self.msg_connect((self.socket_pdu, 'pdus'), (self.pdu_to_tagged_stream, 'pdus'))
         self.connect(self.pdu_to_tagged_stream,
-                     self.unpacker,
-                     self.stream_to_vector,
-                     self.codec2_decode,
-                     self.short_to_float,
+                     self.codec2_decoder,
                      self.audio_sink)
 
 
